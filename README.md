d82959de0713f0a0606444b5eb546249632fc7c1
Bryan Vaz <bryan@imitadvisory.com>
2018-04-01 16:52:15 +0800

    inital commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# AWS CLI in Docker in DOcker
+
+Containerized AWS Base and Elastic Beanstalk Command Line Interface on Node to avoid
+requiring the awscli and awsebcli to be installed on CI machines.
+
+Automated build on CicleCi
+
+#### TODO
+
+## Usage
+
+Configure:
+
+```
+export AWS_ACCESS_KEY_ID="<id>"
+export AWS_SECRET_ACCESS_KEY="<key>"
+export AWS_DEFAULT_REGION="<region>"
+```
+
+
+## References
+
+EB CLI Docs: http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3.html
+
+
+Copyright 2018 Bryan Vaz <contact@pad39digital.com>
---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# Loop through each node variant
+filename="versions"
+while read -ra variants
+do
+  # set up variables
+  node_version="${variants[0]}"
+  base_tag="pad39/node-aws:$node_version"
+  tags=("${variants[@]:1}")
+  echo "processing for node $node_version ..."
+
+  # Generate Docker File
+  echo "  generating docker file ..."
+  docker_filename="Dockerfile.build.$node_version"
+  $(cat Dockerfile.template | sed  -E 's/##NODE_VERSION##/'"$node_version"'/g' > "$docker_filename")
+
+  # Build using docker file
+  echo "  building docker image ..."
+  exec "docker build -t $base_tag -f $docker_filename"
+
+  # Push to Docker Hub
+  for variant in "${variants[@]}"
+  do
+    tag_v="pad39/node-aws:$variant"
+    echo "tagging and pushing for tag: $tag_v"
+    exec "docker tag $base_tag $tag_v"
+    exec "docker push $tag_v"
+  done
+    
+done <<< "$(cat versions | sed -E 's/,/ /g')"
+
+# while IFS='' read -r line || [[ -n "$line" ]]; do
+#     echo "Text read from file: $line"
+# done < "$1"
+# read -ra variants <<< "$(cat versions | sed -E 's/,/ /g')"
+
+
